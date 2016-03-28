866408556251be36f76d9af57e691368721b0253
Matt <matt-hegarty@sbcglobal.net>
2016-03-28 18:15:09 +0000

    Added Delete user, not called anywhere yet

--- CourseAssignmentSystem-master/app/controllers/users_controller.rb
+++ CourseAssignmentSystem-master/app/controllers/users_controller.rb
@@ -13,6 +13,11 @@ def create
   end
 end
 
+def destroy
+  User.find(params(:user_id).destroy
+  redirect_to '/signup'
+end
+
 private
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password)

2225aabcca03c471acff3d95f664e12ba085dc24
pumak254 <cesar_chavira@att.net>
2016-03-28 22:38:15 +0000

    inserted rspec for sessions

--- CourseAssignmentSystem-master/spec/controllers/sessions_controller_spec.rb
+++ CourseAssignmentSystem-master/spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
+# This is a comment 
+
 RSpec.describe SessionsController, type: :controller do
 
+    describe 'logging in' do
+    it 'should redirect to login page if user input is incorrect' do
+      # test here
+    end
+    it 'should redirect to professor home page if user input is correct' do
+      # test here
+    end
+    end
+    
+    describe 'logging out' do 
+    it 'should redirect to login page if the user presses the Log Out button' do
+      # test here
+    end
+    end
+    
+    describe 'signing up' do 
+    it 'should redirect to sign up page if user input is not complete' do
+      # test here
+    end
+    it 'should redirect to professor home page if user input is complete' do
+      # test here
+    end
+    end
+    
 end

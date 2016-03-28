af41108e897156a80b392724bcbdadbb015cebe3
pumak254 <cesar_chavira@att.net>
2016-03-28 20:50:52 +0000

    wrote templates for testing login implementation (rspec)

--- CourseAssignmentSystem-master/spec/controllers/users_controller_spec.rb
+++ CourseAssignmentSystem-master/spec/controllers/users_controller_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
+    describe 'creating new user' do
+    it 'should have the user in the session hash' do
+        # test here
+    end
+    end
+
 end

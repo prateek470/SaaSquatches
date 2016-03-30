1e0ca8be881ae6b627c037d1db8a4194ec2cb6ee
mankusn <mankusnick@yahoo.com>
2016-03-29 21:53:38 -0500

    DB fix

--- CourseAssignmentSystem-master/db/migrate/20151008055215_create_rooms.rb
+++ CourseAssignmentSystem-master/db/migrate/20151008055215_create_rooms.rb
@@ -2,9 +2,9 @@ class CreateRooms < ActiveRecord::Migration
   def change
     create_table :rooms do |t|
       t.string :room_name
-
+      t.string :buildings
+      t.integer :capacity
       t.timestamps null: false
     end
-	add_foreign_key :rooms, :buildings
   end
 end

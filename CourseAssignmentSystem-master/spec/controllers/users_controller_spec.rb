f79ff081b90d452779c1d04db9c4946cbd214b7d
mankusn <mankusnick@yahoo.com>
2016-03-29 23:09:55 -0500

    rspec tests

--- CourseAssignmentSystem-master/spec/controllers/users_controller_spec.rb
+++ CourseAssignmentSystem-master/spec/controllers/users_controller_spec.rb
@@ -2,11 +2,13 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
-    describe 'creating new user' do
+   describe 'creating new user' do
     it 'should have the user in the session hash' do
-        # test here
-        User.should_recieve(:create!).with(:faculty_id=>"id",:faculy_name=>"name",:email=>"email",:password=>"password")
-        post :users, {:user =>{:faculty_id=>"id",:faculty=>"name", :email=>"email"}}
+    
+    post :create, {:user => {:id => "2", :faculty_name=> "Shell Dylan",:faculty_id=>"40", :email => "mm@gmail.com", :password => "asdf"}}
+    #User.should_receive(:new).with({:id => "2", :first_name => "Slim", :last_name => "Shady", :email => "mm@gmail.com", :password => "asdf"})
+    (response.should redirect_to signup_path)
+    
     end
     end
 

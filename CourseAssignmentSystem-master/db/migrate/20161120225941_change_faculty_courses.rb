5e3c2d1bdd56c545c17872c67c61eb58de76ddb7
Prateek Gupta <prateek470@users.noreply.github.com>
2016-11-25 13:00:27 -0600

    removed course id change

--- CourseAssignmentSystem-master/db/migrate/20161120225941_change_faculty_courses.rb
+++ CourseAssignmentSystem-master/db/migrate/20161120225941_change_faculty_courses.rb
@@ -1,8 +1,5 @@
 class ChangeFacultyCourses < ActiveRecord::Migration
   def change
-    remove_column :faculty_courses, :course1_id
-    remove_column :faculty_courses, :course2_id
-    remove_column :faculty_courses, :course3_id
-    add_column :faculty_courses, :course_id, :integer
+    
   end
 end

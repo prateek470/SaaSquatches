0c491553a89581e3fb064b374df9424e1d58b83a
Prateek Gupta <prateek470@gmail.com>
2016-11-26 21:02:06 -0600

    heroku deploy issue

--- CourseAssignmentSystem-master/app/controllers/home_page_controller.rb
+++ CourseAssignmentSystem-master/app/controllers/home_page_controller.rb
@@ -24,19 +24,19 @@ class HomePageController < ApplicationController
   end
 
   def addcourse
-    @data = Course.select('id,course_name,courseTitle,course_size')
-  	if params[:class] != nil && params[:class][:CourseName] != "" && params[:class][:CourseTitle] != "" && params[:class][:course_size] !=""
+    @data = Course.select('id,course_name,course_title,course_size')
+  	if params[:class] != nil && params[:class][:CourseName] != "" && params[:class][:course_title] != "" && params[:class][:course_size] !=""
   	  flash[:error] = nil
       if !Course.exists?(:course_name => params[:class][:CourseName])
-        Course.create!(:course_name => params[:class][:CourseName], :CourseTitle => params[:class][:CourseTitle], :course_size => params[:class][:course_size])
+        Course.create!(:course_name => params[:class][:CourseName], :course_title => params[:class][:course_title], :course_size => params[:class][:course_size])
         flash[:success]= params[:class][:CourseName] + " added to the courses"
       else
-        Course.where(:course_name => params[:class][:CourseName]).update_all(:CourseTitle => params[:class][:CourseTitle], :course_size => params[:class][:course_size])
+        Course.where(:course_name => params[:class][:CourseName]).update_all(:course_title => params[:class][:course_title], :course_size => params[:class][:course_size])
         flash[:success]= "Course " + params[:class][:CourseName] + " updated."
       end
     elsif params[:class] != nil && params[:class][:CourseName] == ""
       flash[:error]= "Course name cannot be empty!"
-    elsif params[:class] != nil && params[:class][:CourseTitle] == ""
+    elsif params[:class] != nil && params[:class][:course_title] == ""
       flash[:error]= "Course title cannot be empty!"
     elsif params[:class] != nil && params[:class][:course_size] == ""
       flash[:error]= "Course size should be more than 0!"

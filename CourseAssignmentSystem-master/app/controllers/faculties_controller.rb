2c5327e24ecca2a9e32a6ce1323efe491a72a257
Prateek Gupta <prateek470@gmail.com>
2016-11-27 15:41:51 -0600

    edit faculty member update. cannot destroy super admin

--- CourseAssignmentSystem-master/app/controllers/faculties_controller.rb
+++ CourseAssignmentSystem-master/app/controllers/faculties_controller.rb
@@ -7,12 +7,20 @@ class FacultiesController < ApplicationController
   end
   
   def addfaculty
-     
     @permissions = ["User", "Admin"]
   	if params[:class] != nil && params[:class][:FacultyName] != "" && params[:class][:permission] != nil
       if Faculty.exists?(faculty_name: params[:class][:FacultyName],permission: params[:class][:permission])
         flash[:error] = "Faculty already exists."
         redirect_to addfaculty_path
+      elsif Faculty.exists?(faculty_name: params[:class][:FacultyName])
+        if params[:class][:FacultyName] == "Super Admin"
+          flash[:warning] = "Cannot change access level of Super Admin."
+          redirect_to addfaculty_path
+        else
+          Faculty.where(faculty_name: params[:class][:FacultyName]).update_all(faculty_name: params[:class][:FacultyName], permission: params[:class][:permission])
+          flash[:success] = "Faculty member Dr. "+params[:class][:FacultyName]+" permission changed to "+params[:class][:permission]
+          redirect_to addfaculty_path
+        end
       else
     		Faculty.create!(faculty_name: params[:class][:FacultyName], permission: params[:class][:permission])
     		flash[:success] = "New Faculty Member Dr. "+params[:class][:FacultyName]+" added."
@@ -28,7 +36,13 @@ class FacultiesController < ApplicationController
   def destroy
     @id = params[:id]
     @faculty = Faculty.find_by id: @id
-    @faculty.destroy
-    redirect_to '/faculties'
+    if @faculty != nil && @faculty.permission == 'Admin' && @faculty.faculty_name == 'Super Admin'
+      flash[:error] = "You cannot delete the Super Admin."
+      redirect_to '/faculties'
+    else
+      flash[:success] = "Faculty member Dr. "+@faculty.faculty_name+" deleted."
+      @faculty.destroy
+      redirect_to '/faculties'
+    end
   end
 end

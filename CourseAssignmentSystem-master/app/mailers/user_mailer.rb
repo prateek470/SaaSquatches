ede7dccbbaec1268195125490caa15949569d8d3
Prateek Gupta <prateek470@gmail.com>
2016-12-03 18:56:01 -0600

    added password reset

--- CourseAssignmentSystem-master/app/mailers/user_mailer.rb
+++ CourseAssignmentSystem-master/app/mailers/user_mailer.rb
@@ -5,4 +5,9 @@ class UserMailer < ActionMailer::Base
 	  @user = user
 	  mail(:to => "#{user.faculty_name} <#{user.email}>", :subject => "Registration Confirmation")
 	end
+
+	def password_reset(user)
+	  @user = user
+	  mail :to => user.email, :subject => "Password Reset"
+	end
 end
